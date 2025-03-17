c521e32fad991ee27571c9a4739caf32a4aea919
shivakumar <shivamahajan055@gmail.com>
2025-03-17 11:40:11 +0530

    user and product relationship

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const errorController = require("./controllers/error");
+const User = require("./models/user");
+const Product = require("./models/product");
 
 const app = express();
 
@@ -15,14 +17,42 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
-  .then((res) => {
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: "Max",
+        email: "max@gmail.com",
+      });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
